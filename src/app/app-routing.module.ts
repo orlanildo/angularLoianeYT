7e82205f46a1c0f87a3cdabcfd2e46e3ee1f4925
orlanildo <orlanildoaldo@hotmail.com>
2021-07-20 16:00:13 -0300

    v9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'reactive-search' },
-  { path: 'courses', loadChildren: './courses/courses.module#CoursesModule' },
-  { path: 'rxjs-poc', loadChildren: './unsubscribe-rxjs/unsubscribe-rxjs.module#UnsubscribeRxjsModule' },
-  { path: 'unpload', loadChildren: './unpload-file/unpload-file.module#UnploadFileModule' },
-  { path: 'reactive-search', loadChildren: './reactive-search/reactive-search.module#ReactiveSearchModule' },
+  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) },
+  { path: 'rxjs-poc', loadChildren: () => import('./unsubscribe-rxjs/unsubscribe-rxjs.module').then(m => m.UnsubscribeRxjsModule) },
+  { path: 'unpload', loadChildren: () => import('./unpload-file/unpload-file.module').then(m => m.UnploadFileModule) },
+  { path: 'reactive-search', loadChildren: () => import('./reactive-search/reactive-search.module').then(m => m.ReactiveSearchModule) },
 ];
 
 @NgModule({
